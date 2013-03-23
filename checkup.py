36a7b7916b4fb4bbd7522fb336d3d76f642ec718
Chris Kuehl <chris@techxonline.net>
2013-03-22 22:25:37 -0400

    Add reporting of which sites are down as printed output.

--- checkup.py
+++ checkup.py
@@ -7,16 +7,33 @@ from socket import timeout
 # methods
 def check_all_sites():
 	all_sites = get_all_sites()
+	failed_sites = []
 	
 	for site in all_sites:
-		check_site(site)
-			
+		status = check_site(site)
+		
+		if (not(status['up'])):
+			failed_sites.append({
+				'site': site,
+				'failed_urls': status['failed_urls']
+			})
+	
+	if (len(failed_sites) > 0):
+		print('Some sites are down:')
+		
+		for site in failed_sites:
+			print("\t{} ({}/{} URLs failed)".format(site['site']['title'], len(site['failed_urls']), len(site['site']['urls'])))
+	else:
+		print('All sites were up.')
+
+
 def get_all_sites():
 	file = open('sites.json')
 	return json.load(file)
 
 def check_site(site):
 	print("Now checking site {}...".format(site['title']))
+	failed_urls = []
 	
 	for url in site['urls']:
 		print("\tChecking: {}...".format(url), end = '')
@@ -25,6 +42,15 @@ def check_site(site):
 			print("up!")
 		else:
 			print("down!")
+			failed_urls.append(url)
+	
+	if (len(failed_urls) > 0):
+		return {
+			'up': False,
+			'failed_urls': failed_urls
+		}
+	else:
+		return {'up': True}
 
 def url_up(url):
 	try: 
@@ -37,5 +63,9 @@ def url_up(url):
 		return False
 	else:
 		return True
+
+def alert_site_down(urls, site):
+	print("")
+	
 # main program
 check_all_sites()
