b044653fee58f849ccff160c40010235c48283e4
Chris Kuehl <chris@techxonline.net>
2013-03-22 23:12:50 -0400

    Starting to add email body generation.

--- checkup.py
+++ checkup.py
@@ -70,10 +70,20 @@ def url_up(url):
 def alert_sites_down(sites):
 	# prepare an email message
 	sites_title_list = ""
-	
+	body = ""
+		
 	for site in sites:
 		sites_title_list += site['site']['title'] + ", "
-	
+		body += site['site']['title'] + ":\n"
+		
+		# display URLS, including those that are up
+		for url in site['site']['urls']:
+			url_is_down = url in site['failed_urls']
+			body += "\t{}: {}\n".format(url, ("DOWN" if url_is_down else "up"))
+		
+		body += "\n"
+			
+	print(body)
 	sites_title_list = sites_title_list[:-2] # chop off the trailing comma and space
 	
 	subject = "SITES DOWN ({}) {}: {}".format(len(sites), strftime("%I:%M %p", gmtime()), sites_title_list)
